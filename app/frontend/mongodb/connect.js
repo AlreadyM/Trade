36e7f01849fca51edb5a52e93fae94193009c278
AlreadyM <617054896@qq.com>
2018-12-11 23:31:20 +0800

    add the notice for start mongo on macos with brew

--- app/frontend/mongodb/connect.js
+++ app/frontend/mongodb/connect.js
@@ -1,3 +1,11 @@
+/**
+ * To have launchd start mongodb now and restart at login:
+    brew services start mongodb
+    Or, if you don't want/need a background service you can just run:
+    mongod --config /usr/local/etc/mongod.conf
+ */
+
+
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
