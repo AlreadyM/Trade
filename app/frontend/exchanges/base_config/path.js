03d59003c8ce83823248e85fd12cfc4ebabf545c
华 <hua@huadeMac-mini.local>
2018-12-18 11:09:38 +0800

    clean up for get data async

--- app/frontend/exchanges/base_config/path.js
+++ app/frontend/exchanges/base_config/path.js
@@ -4,6 +4,11 @@
 	 // };
 
  // base on self file path for mac
-	 module.exports = {
-	 	exchangesPath:'/Users/chunhua/WorkSpace/Trade/app/frontend/exchanges/'
-	 };
+	//  module.exports = {
+	//  	exchangesPath:'/Users/chunhua/WorkSpace/Trade/app/frontend/exchanges/'
+	//  };
+	
+ // base on self file path for mac
+ module.exports = {
+	exchangesPath:'/Users/hua/workspace/Trade/app/frontend/exchanges/'
+};
